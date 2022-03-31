ea172a2f73a6c2eb77f10974ce939bcb9f41ab60
Ahmad Didik Setiyadi <adidiksetiyadi@gmail.com>
2022-03-31 23:47:05 +0700

    kurang colect user data

---
+++ lib/answer.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class Answer extends StatelessWidget {
+  // Answer({Key? key}) : super(key: key);
+
+  final String _answerText;
+  final VoidCallback _selectHandler;
+
+  Answer(this._answerText, this._selectHandler);
+
+  answerColor() {
+    if (_answerText == 'Red')
+      return Colors.red;
+    else if (_answerText == 'Blue')
+      return Colors.blue;
+    else if (_answerText == 'Green')
+      return Colors.green;
+    else
+      return Colors.blue[400];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            primary: answerColor(),
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        child: Text(_answerText),
+        onPressed: () {
+          // window.alert('Answer 1');
+          _selectHandler();
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,46 +9,105 @@ class MyApp extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
-    setState(() {
-      questionIndex++;
-    });
-    if (questionIndex > 2) {
-      questionIndex = 0;
+  final questions = [
+    {
+      'question': "What's your favorite color?",
+      'answers': ['Red', 'Blue', 'Green'],
+    },
+    {
+      'question': "What's your favorite animal?",
+      'answers': ['Dog', 'Cat', 'Rabbit'],
+    },
+    {
+      'question': "What's your favorite food?",
+      'answers': ['Pizza', 'Pasta', 'Cake'],
     }
-    print(questionIndex);
+  ];
+
+  var jawabanPengguna = [];
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    // simpan jawaban pengguna
+    var _ = questions[_questionIndex - 1]['answers'] as List<String>;
+    jawabanPengguna.add(_.elementAt(0));
+
+    print(_questionIndex);
+    print(jawabanPengguna);
   }
 
   @override
   Widget build(BuildContext context) {
-    final question = [
-      "What's your favorite color?",
-      "What's your favorite animal?",
-      "What's your favorite food?"
-    ];
+    // questions with maps => in python is dictionary
+
+    // final answer_widget = (questions[_questionIndex]['answers'] as List<String>)
+    //     .map((e) => Answer(e, _answerQuestion))
+    //     .toList();
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My first app'),
-        ),
-        body: Column(children: [
-          Text(
-            question[questionIndex],
+          appBar: AppBar(
+            title: const Text('My first app'),
           ),
-          ElevatedButton(
-              child: const Text('Answer 1'), onPressed: answerQuestion),
-          ElevatedButton(
-              child: const Text('Answer 2'), onPressed: answerQuestion),
-        ]),
-      ),
+          body: _questionIndex < questions.length
+              ? Column(
+                  children: [
+                    Question(
+                      questions[_questionIndex]['question'] as String,
+                    ),
+                    // Column(
+                    //   children: answer_widget,
+                    // )
+                    ...(questions[_questionIndex]['answers'] as List<String>)
+                        .map((answer) {
+                      return Answer(answer, _answerQuestion);
+                    }).toList()
+                  ],
+                )
+              : Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                          'Jawaban Anda:' +
+                              '\nWarna Favorite: ' +
+                              jawabanPengguna.elementAt(0) +
+                              '\nHewan Favorite: ' +
+                              jawabanPengguna.elementAt(1) +
+                              '\nMakanan Favorite: ' +
+                              jawabanPengguna.elementAt(2) +
+                              '\n',
+                          style: TextStyle(fontSize: 20),
+                          textAlign: TextAlign.center),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.blue[400],
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 20),
+                            textStyle: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        child: Text('Restart'),
+                        onPressed: () {
+                          setState(() {
+                            _questionIndex = 0;
+                          });
+                          jawabanPengguna = [];
+                        },
+                      )
+                    ],
+                  ),
+                )),
     );
   }
 }
